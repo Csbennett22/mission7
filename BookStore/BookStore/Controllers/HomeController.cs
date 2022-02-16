ed67e92ff0e99fbe5c7e5857cfe58a0796dd7d24
Csbennett22 <90526059+Csbennett22@users.noreply.github.com>
2022-02-15 21:12:01 -0700

    One

---
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using BookStore.Models;
+using BookStore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+        public HomeController(IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1) //set the default page to 1 & DO NOT USE page as this variable name
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books //.ToList() //we changed the data that is passed in; it is no longer a list
+                    .OrderBy(b => b.Title)
+                    .Skip(pageSize * (pageNum - 1)) // skip certain number of records
+                    .Take(pageSize), // take a certain number of records
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    ProjectsPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
+        }
+    }
+}
